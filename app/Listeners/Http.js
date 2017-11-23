90ee6d5396fa529bcfd38fb2128e638ddbf768f8
Glevinzon D. Dapal <glevinzon.dapal@usep.edu.ph>
2017-11-23 19:04:12 +0800

    listeners

--- app/Listeners/Http.js
+++ app/Listeners/Http.js
@@ -1,28 +1,8 @@
 'use strict'
 
-const Youch = use('youch')
-
 const Http = exports = module.exports = {}
 
 Http.handleError = async (error, request, response) => {
-
-  const status = error.status || 500
-
-  /**
-   * PRODUCTION REPORTER
-   */
-  console.error(error.stack)
-
-  let jsonResponse = {
-    code: status,
-    message: error.message
-  }
-
-  Response.macro('sendStatus', function (status) {
-    this.status(status).send(jsonResponse)
-  })
-  // await response.status(status).send({ error: jsonResponse })
-}
-
-Http.onStart = async () => {
+  console.log(error)
+  response.status(error.status).send(error.message)
 }
