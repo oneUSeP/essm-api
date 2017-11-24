f17af8d93dac8ac56c652c309a29a4b6def704fb
Glevinzon D. Dapal <glevinzon.dapal@usep.edu.ph>
2017-11-24 10:44:42 +0800

    removed listeners

--- app/Listeners/Http.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const Http = exports = module.exports = {}
-
-Http.handleError = async (error, request, response) => {
-  console.log(error)
-  response.status(error.status).send(error.message)
-}
